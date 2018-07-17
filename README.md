7ae64d75274029f0618f8d674ab1320d348efc95
Christopher Zielinski <ckz8780@gmail.com>
2018-07-17 14:27:52 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Python Async
+
+Demonstrates some of the core improvements achieved byusing asynchronous programming in Python 3.5+
---
+++ async_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class AsyncAppConfig(AppConfig): # CHANGE THIS TO YOUR APP CLASS (e.g. new_app => NewAppConfig)
+    name = 'async_app' # CHANGE THIS TO YOUR APP NAME
